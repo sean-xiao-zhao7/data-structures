e43642aa05f1fd1f40ad5f22210b907b1ddfa5eb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-10-20 16:22:09 -0400

    Add node class

--- linked_list.py
+++ linked_list.py
@@ -2,12 +2,21 @@ from copyreg import constructor
 
 
 class LinkedList:
-    def __init__(self, node_value):
-        self.head = node_value
-        self.next = None
+    def __init__(self, initial_value):
+        self.head = Node(initial_value)
+        self.tail = self.head
 
     def __str__(self) -> str:
         return str(self.head)
+
+
+class Node:
+    def __init__(self, value, next=None) -> None:
+        self.value = value
+        self.next = next
+
+    def __str__(self) -> str:
+        return str(self.value)
 
 
 if __name__ == "__main__":
